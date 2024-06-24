e1352d2ddc82c0a6bb93bd5b566f87a9dc101c5a
Federico <fkraft@mail.austral.edu.ar>
2024-06-24 20:16:04 -0300

    Finished resolutionTests

--- src/test/java/edu/austral/ingsis/math/ResolutionTest.java
+++ src/test/java/edu/austral/ingsis/math/ResolutionTest.java
@@ -27,48 +27,52 @@ public class ResolutionTest {
   /** Case (9 / 2) * 3 */
   @Test
   public void shouldResolveSimpleFunction3() {
-    final Double result = 13.5;
-
-    assertThat(result, equalTo(13.5d));
+    final Double expected = 13.5;
+    Function division = new Division(new FixedValue(9), new FixedValue(2));
+    Function function = new Multiplication(division, new FixedValue(3));
+    Double result = function.evaluate(null);
+    assertThat(result, equalTo(expected));
   }
-
   /** Case (27 / 6) ^ 2 */
   @Test
   public void shouldResolveSimpleFunction4() {
     final Double result = 20.25;
-
-    assertThat(result, equalTo(20.25d));
+    Function division = new Division(new FixedValue(27), new FixedValue(6));
+    Function function = new Pwr(division, new FixedValue(2));
+    assertThat(result, equalTo(function.evaluate()));
   }
 
   /** Case 36 ^ (1/2) */
   @Test
   public void shouldResolveSimpleFunction5() {
     final Double result = 6d;
-
-    assertThat(result, equalTo(6d));
+    Function pwr = new Pwr(new FixedValue(36), new Division(new FixedValue(1), new FixedValue(2)));
+    assertThat(result, equalTo(pwr.evaluate()));
   }
 
   /** Case |136| */
   @Test
   public void shouldResolveSimpleFunction6() {
     final Double result = 136d;
-
-    assertThat(result, equalTo(136d));
+    Function function = new AbsValue(new FixedValue(136));
+    assertThat(result, equalTo(function.evaluate()));
   }
 
   /** Case |-136| */
   @Test
   public void shouldResolveSimpleFunction7() {
     final Double result = 136d;
-
-    assertThat(result, equalTo(136d));
+    Function function = new AbsValue(new FixedValue(-136));
+    assertThat(result, equalTo(function.evaluate()));
   }
 
   /** Case (5 - 5) * 8 */
   @Test
   public void shouldResolveSimpleFunction8() {
-    final Double result = 0d;
-
-    assertThat(result, equalTo(0d));
+    final Double expected = 0d;
+    Function substraction = new Substraction(new FixedValue(5), new FixedValue(5));
+    Function function = new Multiplication(substraction, new FixedValue(8));
+    Double result = function.evaluate(null);
+    assertThat(result, equalTo(expected));
   }
 }
